3378a97ad2c7c76d84b952f504f6a420782fb195
Fatuma Dahir <faridhfatush2024@gmail.com>
2025-05-02 10:41:40 +0300

    required deliverables done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const taskText = input.value.trim();
+    if (taskText !== "") {
+      const li = document.createElement("li");
+      li.textContent = taskText;
+      taskList.appendChild(li);
+      input.value = ""; // Clear the input field
+    }
+  });
 });
